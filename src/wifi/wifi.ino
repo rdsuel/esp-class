450d4e0df36cc670e2b187e7e093110c9ebc5bc6
Paul Goodjohn <paul.goodjohn@geappliances.com>
2018-10-02 10:55:24 -0400

    Add simple wifi network

---
+++ src/wifi/wifi.ino
@@ -0,0 +1,33 @@
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "HBL";
+const char* password = "C0A80400C0A804FF";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // Connect to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}
